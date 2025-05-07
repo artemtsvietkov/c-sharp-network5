d475e3ea89518d8d2d0b01e39c0d7a4f87e3123f
アルテム <143085970+artemtsvietkov@users.noreply.github.com>
2025-05-07 10:13:50 +0200

    .zs

--- Socket/Program.cs
+++ Socket/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Net.Sockets;
+using System.Net.Sockets;
 using System.Text;
 
 try
@@ -6,7 +6,7 @@ try
     var client = new TcpClient("127.0.0.1", 13001);
     var stream = client.GetStream();
     byte[] buffer = new byte[1024];
-    Console.WriteLine("Подключен к серверу.");
+    Console.WriteLine("Connected to the server.");
 
     while (true)
     {
@@ -14,10 +14,10 @@ try
         if (bytesRead == 0) break;
 
         string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-        Console.WriteLine("Сообщение от сервера: " + message);
+        Console.WriteLine("Message from the server: " + message);
     }
 }
 catch
 {
-    Console.WriteLine("Ошибка подключения к серверу.");
+    Console.WriteLine("Failed to connect to the server.");
 }
